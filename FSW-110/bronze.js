2b590424cba82e666940bb42f685915df44ea08f
BrigitteOgoke <BrigitteOgoke>
2019-10-30 02:11:06 -0500

    add work olympics

---
+++ FSW-110/bronze.js
@@ -0,0 +1,15 @@
+// 18 and older can go to movies
+
+var person = {
+    name: Bobby;
+    age: 12;
+}
+
+{
+    if (personName === 'b')
+    {return true
+    } else if (personname != 'b'){
+        return false
+    }
+}
+
---
+++ FSW-110/olympicss.js
@@ -0,0 +1,21 @@
+// preliminaries
+
+if(5 > 3){
+    System.out.println("Is greater than!");
+    } 
+
+
+//length
+
+if(cat === 3){
+    System.out.println("Is the length");
+}
+
+//
+if(cat === dog){
+    System.out.println("Is the same");
+    }else{
+    System.out.println("Not the same");
+}
+
+//
