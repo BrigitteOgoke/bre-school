c314161e5dc54fdf890d16b77cdb77d95233862a
BrigitteOgoke <BrigitteOgoke>
2019-11-11 23:00:03 -0600

    week 3 keep dry

---
+++ FSW-105/week 3/keepdry.js
@@ -0,0 +1,26 @@
+// function sum( num1, num2){
+//     return num1 + num2
+// }
+
+// var result = sum(3,5);
+
+// console.log(result)
+
+
+// var array1 = [1, 3, 2];
+
+// console.log(Math.max(...array1));
+
+// function isEven(x) {
+//     return x % 2 === 0;
+//     }
+//     var result = isEven(2);
+//     console.log(result)
+
+ function n(string1, string2) {
+     return "string1" + "string2"
+ }
+ var i = 0; i <= n.length; i++;
+ {
+    console.log(i)
+ }
