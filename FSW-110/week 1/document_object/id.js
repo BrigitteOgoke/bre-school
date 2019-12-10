fa346d800a65f40624db64ec2dd7fd4a93e68f81
BrigitteOgoke <BrigitteOgoke>
2019-12-10 02:35:52 -0600

    selecting elements

---
+++ FSW-110/week 1/document_object/id.js
@@ -0,0 +1,4 @@
+var myText= document.getElementById('random-header');
+var text= document.createTextNode('Hello Wolrd');
+myText.appendChild(text);
+document.getElementById('random-header').appendChild(text);
